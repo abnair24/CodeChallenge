4980702b7172fd85cd2faadd819dc36450ea28d9
Aswathy <aswathy.sreenath9@gmail.com>
2017-02-17 22:11:19 +0530

    PriorityQ

---
+++ src/main/java/com/abn/dsalgos/PriorityQLinkList.java
@@ -0,0 +1,106 @@
+package com.abn.dsalgos;
+
+import com.abn.dsalgos.utils.LinkNode;
+
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
+
+/**
+ * Created by aswathyn on 16/02/17.
+ */
+public class PriorityQLinkList<T extends Comparable<T> > {
+
+    private LinkNode<T> head;
+    private int size ;
+
+    public PriorityQLinkList() {
+        head = null;
+        size =0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void insert(T value) {
+        LinkNode<T> newLink = new LinkNode<>(value);
+
+        LinkNode<T> prev = null ;
+        LinkNode<T> current = head;
+
+//        if(size==0) {
+//            head=newLink;
+//        }
+//        else if(size==1)
+//        {
+//            if(current.data.compareTo(value) <=0)
+//            {
+//                prev=head;
+//                head=newLink;
+//                head.next=prev;
+//            }
+//        }
+//        else {
+//            boolean newLinkInserted=false;
+//            while (current!=null)
+//            {
+//                if(current.data.compareTo(value) > 0)
+//                {
+//                    prev=current;
+//                    current=current.next;
+//                }
+//                else
+//                {
+//
+//                    prev.next=newLink;
+//                    newLink.next=current;
+//                    newLinkInserted=true;
+//                    break;
+//                }
+//            }
+//            if(!newLinkInserted)
+//            {
+//                prev.next=newLink;
+//            }
+//        }
+
+
+        while(current !=null && current.data.compareTo(value) > 0) {
+            prev = current;
+            current = current.next;
+        }
+
+        if(prev == null) {
+            head = newLink;
+        } else {
+            prev.next = newLink;
+        }
+        newLink.next =current;
+        size ++;
+    }
+
+    public T delete() {
+        if(isEmpty()){
+            throw new NoSuchElementException();
+        } else {
+            LinkNode<T> temp = head;
+            head = head.next;
+            size--;
+            return temp.data;
+        }
+
+    }
+
+    public T peek() {
+        if(isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            return head.data;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+}
---
+++ src/test/java/com/abn/dsalgos/PriorityQLinkListTest.java
@@ -0,0 +1,98 @@
+package com.abn.dsalgos;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Created by aswathyn on 16/02/17.
+ */
+
+public class PriorityQLinkListTest {
+
+    private PriorityQLinkList<Employee> employeeLinkList;
+
+    @BeforeMethod
+    public void setup() throws Exception {
+        employeeLinkList = new PriorityQLinkList<>();
+    }
+
+    @Test
+    public void priorityQSizeTest() {
+        employeeLinkList.insert(new Employee(10,"A"));
+        employeeLinkList.insert(new Employee(5,"B"));
+        employeeLinkList.insert(new Employee(15,"C"));
+        employeeLinkList.insert(new Employee(12,"D"));
+        Assert.assertEquals(4,employeeLinkList.size());
+    }
+
+    @Test(expectedExceptions = NoSuchElementException.class)
+    public void priorityQEmptyTest() {
+        employeeLinkList.peek();
+    }
+
+    @Test
+    public void priorityQDeleteTest() {
+        employeeLinkList.insert(new Employee(10,"A"));
+        employeeLinkList.insert(new Employee(1,"B"));
+        employeeLinkList.insert(new Employee(5,"C"));
+        Assert.assertEquals(1,employeeLinkList.delete().getId());
+    }
+
+    @Test
+    public void priorityQpeekTest() {
+        employeeLinkList.insert(new Employee(10,"A"));
+        employeeLinkList.insert(new Employee(1,"B"));
+        employeeLinkList.insert(new Employee(5,"C"));
+        Assert.assertEquals(1,employeeLinkList.peek().getId());
+    }
+
+    @Test(expectedExceptions = NoSuchElementException.class)
+    public void priorityQEmptyDeleteTest() {
+        employeeLinkList.delete();
+    }
+}
+
+class Employee implements Comparable<Employee>{
+
+    int id;
+    String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        return o.getId()-this.getId();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.id);
+    }
+}
+
+
+
+
+
