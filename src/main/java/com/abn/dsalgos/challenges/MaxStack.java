0c6441a133ca694bf0a9bf664097e89eed52acce
Aswathy <aswathy.sreenath9@gmail.com>
2017-02-18 12:40:24 +0530

    Max Stack

---
+++ src/main/java/com/abn/dsalgos/challenges/MaxStack.java
@@ -0,0 +1,51 @@
+package com.abn.dsalgos.challenges;
+
+import java.util.Stack;
+
+/**
+ * Created by aswathyn on 18/02/17.
+ */
+
+class Wrapper{
+    int data;
+    int maxData;
+
+    public Wrapper(int data, int maxData) {
+        this.data = data;
+        this.maxData = maxData;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getMaxData() {
+        return maxData;
+    }
+
+    public void setMaxData(int maxData) {
+        this.maxData = maxData;
+    }
+}
+
+public class MaxStack {
+
+    private Stack<Wrapper> stack=new Stack<>();
+
+    public void insert(int data) {
+        int max= stack.isEmpty()? data : data>getMaxData() ? data : getMaxData();
+        stack.push(new Wrapper(data,max));
+    }
+
+    public int getMaxData() {
+        return stack.peek().getMaxData()  ;
+    }
+
+    public int pop() {
+        return stack.pop().getData();
+    }
+}
---
+++ src/test/java/challenges/MaxStackTest.java
@@ -0,0 +1,51 @@
+package challenges;
+
+import com.abn.dsalgos.challenges.MaxStack;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.EmptyStackException;
+
+/**
+ * Created by aswathyn on 18/02/17.
+ */
+public class MaxStackTest {
+
+    private MaxStack maxStack;
+
+
+    @BeforeMethod
+    public void setup() throws Exception {
+        maxStack = new MaxStack();
+    }
+
+    @Test
+    public void maxStackTest() {
+        maxStack.insert(10);
+        maxStack.insert(20);
+        maxStack.insert(5);
+        maxStack.insert(8);
+        maxStack.insert(50);
+        maxStack.insert(-1);
+        maxStack.insert(30);
+        maxStack.insert(100);
+        maxStack.insert(0);
+        Assert.assertEquals(100, maxStack.getMaxData());
+    }
+
+    @Test(expectedExceptions = EmptyStackException.class)
+    public void maxOnEmptyStackTest() {
+        maxStack.getMaxData();
+    }
+
+    @Test
+    public void maxOnSameValueStack() {
+        maxStack.insert(10);
+        maxStack.insert(30);
+        maxStack.insert(20);
+        maxStack.insert(30);
+        maxStack.insert(15);
+        Assert.assertEquals(30,maxStack.getMaxData());
+    }
+}
