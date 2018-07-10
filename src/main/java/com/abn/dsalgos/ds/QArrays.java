1d74942f8255553ba56f7e4d9c0a70ad2e22cc56
Aswathy <aswathy.sreenath9@gmail.com>
2018-07-10 13:54:13 +0530

    Queue Array

---
+++ src/main/java/com/abn/dsalgos/ds/QArrays.java
@@ -0,0 +1,41 @@
+package com.abn.dsalgos.ds;
+
+import java.util.*;
+
+public class QArrays<T> {
+    private int size ;
+    private static final int MAX_SIZE=10;
+    private Object[] element ;
+    private int front;
+    private int rear;
+
+
+    public QArrays() {
+        element = new Object[MAX_SIZE];
+        size = 0;
+        front = 0;
+        rear = -1;
+    }
+
+    public boolean isEmpty() {
+        return (element.length == 0);
+    }
+
+    public void enqueue(T t)  {
+        if(size == element.length) {
+            int newSize = element.length * 2;
+            element = Arrays.copyOf(element,newSize);
+        }
+        element[size++] = t;
+    }
+
+    public T dequeue() {
+        T t = (T) element[front];
+        front++;
+        if(front == element.length -1) {
+            front =0;
+        }
+        size--;
+        return t;
+    }
+}
---
+++ src/test/java/com/abn/dsalgos/ds/QArraysTest.java
@@ -0,0 +1,21 @@
+package com.abn.dsalgos.ds;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class QArraysTest {
+
+    private QArrays<Integer> qArrays;
+
+    @Test
+    public void enqueueTest() throws Exception {
+        qArrays = new QArrays<Integer>();
+        qArrays.enqueue(1);
+        qArrays.enqueue(2);
+        qArrays.enqueue(3);
+
+        Assert.assertEquals(Integer.parseInt(qArrays.dequeue().toString()),1);
+        Assert.assertEquals(Integer.parseInt(qArrays.dequeue().toString()),2);
+    }
+
+}
