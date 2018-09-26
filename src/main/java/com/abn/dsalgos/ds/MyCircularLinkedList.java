76047f7819af559b8378f562b3d60a27a7db33c5
Aswathy <aswathy.sreenath9@gmail.com>
2018-09-26 20:11:43 +0530

    circularlinkedlist

---
+++ src/main/java/com/abn/dsalgos/ds/MyCircularLinkedList.java
@@ -0,0 +1,80 @@
+package com.abn.dsalgos.ds;
+
+import com.abn.dsalgos.utils.LinkNode;
+import sun.awt.image.ImageWatched;
+
+public class MyCircularLinkedList<T> {
+
+    private LinkNode<T> first;
+    private LinkNode<T> last;
+    private int size ;
+
+    public MyCircularLinkedList() {
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void insertFirst(T value) {
+
+        LinkNode<T> newLink = new LinkNode<>(value);
+
+        if(isEmpty()) {
+            first = newLink;
+            last = newLink;
+            newLink.next = first;
+        } else {
+            LinkNode<T> temp = first;
+            first = newLink;
+            newLink.next = temp;
+            last.next = first;
+        }
+        size++;
+    }
+
+
+    public void insertLast(T value) {
+        LinkNode<T> newLink  = new LinkNode<>(value);
+        if(isEmpty()) {
+            insertFirst(value);
+        } else {
+            LinkNode<T> temp = last;
+            last = newLink;
+            temp.next = newLink;
+            last.next = first;
+        }
+        size++;
+    }
+
+    public void deleteFirst() {
+        if(isEmpty()) {
+            System.out.println("Empty list");
+        } else {
+            first = first.next;
+            last.next = first;
+            size--;
+        }
+    }
+
+    public T elementAt(int index) {
+        LinkNode<T> temp = first;
+        while(index - 1 !=0) {
+            temp = temp.next;
+            index --;
+        }
+        return temp.data;
+    }
+
+    public void print(){
+        LinkNode<T> temp = first;
+
+        do {
+            System.out.println(temp.data + " ");
+            temp = temp.next;
+        } while(temp!=first);
+    }
+}
---
+++ src/test/java/com/abn/dsalgos/ds/CircularLinkedListTest.java
@@ -0,0 +1,30 @@
+package com.abn.dsalgos.ds;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class CircularLinkedListTest {
+
+    private MyCircularLinkedList<Integer>myCircularLinkedList;
+
+    @BeforeMethod
+    public void setup() throws Exception  {
+        myCircularLinkedList = new MyCircularLinkedList<>();
+    }
+
+    @Test
+    public void test() throws Exception {
+        myCircularLinkedList.insertFirst(1);
+        myCircularLinkedList.insertLast(10);
+        myCircularLinkedList.insertFirst(0);
+        myCircularLinkedList.insertLast(5);
+        myCircularLinkedList.insertLast(6);
+        myCircularLinkedList.insertLast(7);
+        myCircularLinkedList.insertLast(8);
+
+        myCircularLinkedList.print();
+
+        Assert.assertEquals((int) myCircularLinkedList.elementAt(5),6);
+    }
+}
