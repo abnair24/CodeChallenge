5d89a37861a0b5dcb164f9df6fc9939b7a066ada
Aswathy <aswathy.sreenath9@gmail.com>
2018-09-27 14:49:56 +0530

    Reverse LinkedList

---
+++ src/main/java/com/abn/dsalgos/challenges/ReverseLinkedListIterative.java
@@ -0,0 +1,153 @@
+package com.abn.dsalgos.challenges;
+
+import com.abn.dsalgos.utils.LinkNode;
+
+import java.util.NoSuchElementException;
+
+public class ReverseLinkedListIterative<T> {
+
+    private LinkNode<T> first;
+    private LinkNode<T> last;
+    private int size;
+
+    public ReverseLinkedListIterative() {
+        first =null;
+        last = null;
+        size=0;
+    }
+
+    public void insertFirst(T value) {
+        LinkNode<T> newLink = new LinkNode<>(value);
+        if(isEmpty()) {
+            last = newLink;
+        } else {
+            newLink.next = first;
+        }
+        first = newLink;
+        size++;
+    }
+
+    public void insertLast(T value) {
+        LinkNode <T> newLink = new LinkNode<>(value);
+        if(isEmpty()) {
+            first = newLink;
+        } else {
+            last.next = newLink;
+        }
+        last = newLink;
+        size ++;
+
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public T deleteFirst() throws Exception {
+        if(isEmpty()){
+            throw new NoSuchElementException();
+        } else {
+            LinkNode<T> temp = first;
+            first = first.next;
+            size--;
+            return temp.data;
+        }
+    }
+
+    public T find(T value) {
+        LinkNode<T> current = first;
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            while (current.data != value) {
+                if (current.next == null) {
+                    throw new IndexOutOfBoundsException();
+                } else {
+                    current = current.next;
+                }
+            }
+        }
+        return current.data;
+    }
+
+    public T delete(T value) {
+
+        LinkNode<T> current = first;
+        LinkNode<T> prev = first;
+        if(isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            while (current.data != value) {
+                if (current.next == null) {
+                    throw new IndexOutOfBoundsException();
+                } else {
+                    prev = current;
+                    current = current.next;
+                }
+            }
+            if (current == first) {
+                first = first.next;
+            } else {
+                prev.next = current.next;
+            }
+        }
+        return current.data;
+    }
+
+    public T getFirst() {
+        LinkNode<T> temp = first;
+        if(temp == null){
+            throw new NoSuchElementException();
+        } else {
+            return temp.data;
+        }
+    }
+
+    public T getLast() {
+        LinkNode<T> temp = last;
+        if(temp == null) {
+            throw new NoSuchElementException();
+        } else {
+            return temp.data;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public LinkNode getFirstNode() {
+        LinkNode<T> temp = first;
+        if(temp == null ){
+            throw new NoSuchElementException();
+        } else {
+            return temp;
+        }
+    }
+
+    public void print() {
+        LinkNode n = first;
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+
+    public void reverseLinkedList() {
+        LinkNode<T> currentNode = first;
+        LinkNode<T> prevNode = null;
+        LinkNode<T> nextNode = null;
+
+        while(currentNode!=null) {
+            // Getting the next node and storing
+            nextNode = currentNode.next;
+            // linking current node to null (for first case and late to prev nods)
+            currentNode.next = prevNode;
+            //iterating
+            prevNode = currentNode;
+            currentNode = nextNode;
+        }
+        first = prevNode;
+        print();
+    }
+}
---
+++ src/test/java/com/abn/dsalgos/challenges/ReverseLinkedListIterativeTest.java
@@ -0,0 +1,26 @@
+package com.abn.dsalgos.challenges;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ReverseLinkedListIterativeTest {
+
+    private ReverseLinkedListIterative<Integer>reverseLinkedListIterative;
+
+    @BeforeMethod
+    public void setup() throws Exception {
+        reverseLinkedListIterative = new ReverseLinkedListIterative<>();
+    }
+
+    @Test
+    public void test() throws Exception {
+        reverseLinkedListIterative.insertFirst(1);
+        reverseLinkedListIterative.insertLast(2);
+        reverseLinkedListIterative.insertLast(3);
+        reverseLinkedListIterative.insertLast(4);
+        reverseLinkedListIterative.insertLast(5);
+
+        reverseLinkedListIterative.reverseLinkedList();
+    }
+
+}
