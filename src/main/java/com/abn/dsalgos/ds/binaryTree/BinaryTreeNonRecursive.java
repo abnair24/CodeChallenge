becc5691e9b7f4e99d134345f1f88cd1c868a71c
Aswathy <aswathy.sreenath9@gmail.com>
2018-10-17 21:37:05 +0530

    BT Non-recursive

---
+++ src/main/java/com/abn/dsalgos/ds/binaryTree/BinaryTreeNonRecursive.java
@@ -0,0 +1,57 @@
+package com.abn.dsalgos.ds.binaryTree;
+
+import com.abn.dsalgos.utils.MyTreeNode;
+
+import java.util.Stack;
+
+public class BinaryTreeNonRecursive<T> {
+
+    MyTreeNode root;
+
+    public BinaryTreeNonRecursive() {
+        root = null;
+    }
+
+    public void inOrder(MyTreeNode node) {
+        Stack<MyTreeNode> stack = new Stack<>();
+
+        while(true) {
+            while(root !=null){
+                stack.push(root);
+                root = root.left;
+            }
+
+            if(stack.isEmpty()) {
+                return;
+            }
+            root = stack.pop();
+            System.out.println(root.data);
+            root = root.right;
+        }
+    }
+
+    public void preOrder(MyTreeNode node) {
+        Stack<MyTreeNode> stack = new Stack<>();
+
+        while(true) {
+            while(root!=null) {
+                System.out.println(root.data);
+                stack.push(root);
+                root = root.left;
+            }
+
+            if(stack.isEmpty()){
+                return;
+            }
+
+            root = stack.pop();
+            root = root.right;
+        }
+    }
+
+    public void postOrder(MyTreeNode node) {
+        Stack<MyTreeNode> stack = new Stack<>();
+
+
+    }
+}
---
+++ src/test/java/com/abn/dsalgos/ds/binaryTree/BinaryTreeNonRecursiveTest.java
@@ -0,0 +1,36 @@
+package com.abn.dsalgos.ds.binaryTree;
+
+import com.abn.dsalgos.utils.MyTreeNode;
+import org.testng.annotations.Test;
+
+public class BinaryTreeNonRecursiveTest {
+
+    private BinaryTreeNonRecursive<Integer> binaryTreeNonRecursive = new BinaryTreeNonRecursive<>();
+
+    @Test
+    public void inOrderTest() throws Exception {
+        binaryTreeNonRecursive.root = new MyTreeNode(1);
+        binaryTreeNonRecursive.root.left = new MyTreeNode(2);
+        binaryTreeNonRecursive.root.right = new MyTreeNode(3);
+        binaryTreeNonRecursive.root.left.left = new MyTreeNode(4);
+        binaryTreeNonRecursive.root.left.right = new MyTreeNode(5);
+
+        binaryTreeNonRecursive.inOrder(binaryTreeNonRecursive.root);
+    }
+
+    @Test
+    public void preOrderTest() throws Exception {
+        binaryTreeNonRecursive.root = new MyTreeNode(1);
+        binaryTreeNonRecursive.root.left = new MyTreeNode(2);
+        binaryTreeNonRecursive.root.right = new MyTreeNode(3);
+        binaryTreeNonRecursive.root.left.left = new MyTreeNode(4);
+        binaryTreeNonRecursive.root.left.right = new MyTreeNode(5);
+        binaryTreeNonRecursive.root.right.left = new MyTreeNode(6);
+        binaryTreeNonRecursive.root.right.right = new MyTreeNode(7);
+
+        binaryTreeNonRecursive.preOrder(binaryTreeNonRecursive.root);
+
+    }
+
+
+}
