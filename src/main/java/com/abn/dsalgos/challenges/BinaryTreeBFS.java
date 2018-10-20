cf08b0efd9d21919aa190edb6ff208c9d8da1029
Aswathy <aswathy.sreenath9@gmail.com>
2018-10-20 12:59:54 +0530

    Breadth First Search

---
+++ src/main/java/com/abn/dsalgos/challenges/BinaryTreeBFS.java
@@ -0,0 +1,38 @@
+package com.abn.dsalgos.challenges;
+
+import com.abn.dsalgos.utils.MyTreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTreeBFS<T> {
+
+    MyTreeNode root;
+
+    public BinaryTreeBFS() {
+        root = null;
+    }
+
+    public void breadthFirstSearch(MyTreeNode node) {
+        Queue<MyTreeNode> queue = new LinkedList<MyTreeNode>();
+        if (node== null){
+            return;
+        }
+
+        queue.add(node);
+
+        while (!queue.isEmpty()){
+            MyTreeNode temp = queue.remove();
+            System.out.println(temp.data);
+
+            if(temp.left!=null) {
+                queue.add(temp.left);
+            }
+
+            if(temp.right!=null) {
+                queue.add(temp.right);
+            }
+        }
+    }
+
+}
---
+++ src/test/java/com/abn/dsalgos/challenges/BinaryTreeBFSTest.java
@@ -0,0 +1,23 @@
+package com.abn.dsalgos.challenges;
+
+import com.abn.dsalgos.utils.MyTreeNode;
+import org.testng.annotations.Test;
+
+public class BinaryTreeBFSTest {
+
+    private BinaryTreeBFS<Integer>binaryTreeBFS = new BinaryTreeBFS<>();
+
+    @Test
+    public void test() throws Exception {
+        binaryTreeBFS.root = new MyTreeNode(1);
+        binaryTreeBFS.root.left = new MyTreeNode(2);
+        binaryTreeBFS.root.right = new MyTreeNode(3);
+        binaryTreeBFS.root.left.left = new MyTreeNode(4);
+        binaryTreeBFS.root.left.right = new MyTreeNode(5);
+
+        binaryTreeBFS.root.right.left = new MyTreeNode(6);
+        binaryTreeBFS.root.right.right = new MyTreeNode(7);
+
+        binaryTreeBFS.breadthFirstSearch(binaryTreeBFS.root);
+    }
+}
