507531e866b5598e8dd80c039ddab0a1e4f60754
Aswathy <aswathy.sreenath9@gmail.com>
2018-10-31 12:03:26 +0530

    In order successor in BST

---
+++ src/main/java/com/abn/dsalgos/challenges/BST/InOrderSuccessor.java
@@ -0,0 +1,85 @@
+package com.abn.dsalgos.challenges.BST;
+
+import com.abn.dsalgos.utils.MyBSTNode;
+
+/*
+    Find InOrder successor of a node in binary search tree
+
+    HINT : What is Inorder Successor: Inorder successor of a node is the next node in
+    the inorder traversal of the tree. For the last node in a tree,
+    inorder successor will be NULL
+
+    Case 1:  If right subtree of node is not NULL, then successor is the left most child in right subtree
+
+    Case 2:  If right subtree of node is NULL, then succ is one of the ancestors
+ */
+
+public class InOrderSuccessor<T extends Comparable<T>> {
+
+    MyBSTNode<T> root;
+
+    public InOrderSuccessor() {
+        root = null;
+    }
+
+    public void insertNode(MyBSTNode<T> node) {
+        if(root == null) {
+            root = node;
+            return;
+        }
+        MyBSTNode<T> current = root;
+        MyBSTNode<T> parent = null;
+        while(current != null) {
+            parent = current;
+
+            if(node.data.compareTo(current.data)<0) {
+                current = current.left;
+                if(current == null) {
+                    parent.left = node;
+                    return;
+                }
+            } else {
+                current = current.right;
+                if(current == null) {
+                    parent.right = node;
+                    return;
+                }
+            }
+        }
+    }
+
+    public MyBSTNode<T> getSuccessor(MyBSTNode<T> root, MyBSTNode<T> node) {
+
+        MyBSTNode<T> successor = node.right;
+        MyBSTNode<T> current = root;
+
+        /*
+        Case 1:
+         */
+        if(successor != null) {
+            while(successor.left != null) {
+
+                successor = successor.left;
+            }
+            return successor;
+        }
+        /*
+         Case 2:
+         */
+        else {
+            MyBSTNode<T> temp = null;
+
+            while (current != null) {
+                if (node.data.compareTo(current.data) == 0) {
+                    break;
+                } else if (node.data.compareTo(current.data) < 0) {
+                    temp = current;
+                    current = current.left;
+                } else {
+                    current = current.right;
+                }
+            }
+            return temp;
+        }
+    }
+}
---
+++ src/test/java/com/abn/dsalgos/challenges/BST/InOrderSuccessorTest.java
@@ -0,0 +1,43 @@
+package com.abn.dsalgos.challenges.BST;
+
+import com.abn.dsalgos.utils.MyBSTNode;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class InOrderSuccessorTest {
+
+    InOrderSuccessor<Integer>inOrderSuccessor = new InOrderSuccessor<>();
+
+    @Test
+    public void test() throws Exception {
+        MyBSTNode<Integer> node1 = new MyBSTNode<Integer>(100);
+        MyBSTNode<Integer> node2 = new MyBSTNode<>(150);
+        MyBSTNode<Integer> node3 = new MyBSTNode<>(50);
+        MyBSTNode<Integer> node4 = new MyBSTNode<>(25);
+        MyBSTNode<Integer> node5 = new MyBSTNode<>(75);
+        MyBSTNode<Integer> node6 = new MyBSTNode<>(125);
+        MyBSTNode<Integer> node7 = new MyBSTNode<>(175);
+        MyBSTNode<Integer> node8 = new MyBSTNode<>(120);
+        MyBSTNode<Integer> node9 = new MyBSTNode<>(140);
+        MyBSTNode<Integer> node10 = new MyBSTNode<>(160);
+        MyBSTNode<Integer> node11 = new MyBSTNode<>(190);
+
+        inOrderSuccessor.insertNode(node1);
+        inOrderSuccessor.insertNode(node2);
+        inOrderSuccessor.insertNode(node3);
+        inOrderSuccessor.insertNode(node4);
+        inOrderSuccessor.insertNode(node5);
+        inOrderSuccessor.insertNode(node6);
+        inOrderSuccessor.insertNode(node7);
+        inOrderSuccessor.insertNode(node8);
+        inOrderSuccessor.insertNode(node9);
+        inOrderSuccessor.insertNode(node10);
+        inOrderSuccessor.insertNode(node11);
+
+        Assert.assertEquals(inOrderSuccessor.getSuccessor(node1,node5), node1);
+        Assert.assertEquals(inOrderSuccessor.getSuccessor(node1,node4), node3);
+        Assert.assertEquals(inOrderSuccessor.getSuccessor(node1,node6), node9);
+        Assert.assertEquals(inOrderSuccessor.getSuccessor(node1,node2), node10);
+        Assert.assertEquals(inOrderSuccessor.getSuccessor(node1,node11),null);
+    }
+}
