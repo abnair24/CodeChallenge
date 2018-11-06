85befac5af4d5b688aeb4fce6bf09f2f02677b6b
Aswathy <aswathy.sreenath9@gmail.com>
2018-11-06 22:48:50 +0530

    BinaryTree balanced iterative

---
+++ src/main/java/com/abn/dsalgos/challenges/binaryTree/BinaryTreeIsBalanced.java
@@ -0,0 +1,65 @@
+package com.abn.dsalgos.challenges.binaryTree;
+
+import com.abn.dsalgos.utils.MyTreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ Given a binary tree, determine if it is height-balanced.
+ Hint : A binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+ */
+public class BinaryTreeIsBalanced<T> {
+
+    MyTreeNode<T> root ;
+
+    public BinaryTreeIsBalanced() {
+        root = null;
+    }
+
+    public boolean isBalanced(MyTreeNode<T> node) throws Exception {
+        if (node == null) {
+            return true;
+        }
+
+        Queue<MyTreeNode<T>> queue = new LinkedList<>();
+
+        int height = 1;
+        int minHeight = Integer.MAX_VALUE;
+        int maxHeight = Integer.MIN_VALUE;
+
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            int nodeCount = queue.size();
+
+            while (nodeCount > 0) {
+                MyTreeNode<T> temp = queue.poll();
+
+                if (temp.left != null)
+                    queue.add(temp.left);
+                if (temp.right != null)
+                    queue.add(temp.right);
+
+                /*
+                Height diff of left and right subtree at every node needs to be checked and diff <=1
+                Below condition checks when either left node is null or right node becomes null.
+                Test case: 4,5,6
+                 */
+                if (temp.left == null || temp.right == null) {
+                    if (minHeight > height)
+                        minHeight = height;
+                    if (maxHeight < height)
+                        maxHeight = height;
+                }
+
+                nodeCount--;
+            }
+            if (Math.abs(maxHeight - minHeight) > 1) {
+                return false;
+            }
+            height++;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/com/abn/dsalgos/challenges/binaryTree/BinaryTreeIsBalancedTest.java
@@ -0,0 +1,84 @@
+package com.abn.dsalgos.challenges.binaryTree;
+
+import com.abn.dsalgos.utils.MyTreeNode;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class BinaryTreeIsBalancedTest {
+
+    BinaryTreeIsBalanced<Integer> binaryTreeIsBalanced;
+
+    @BeforeMethod(alwaysRun = true)
+    public void beforeMethod() throws Exception {
+       binaryTreeIsBalanced = new BinaryTreeIsBalanced<>();
+    }
+
+    @Test
+    public void test() throws Exception{
+        binaryTreeIsBalanced.root = new MyTreeNode<>(1);
+        binaryTreeIsBalanced.root.left = new MyTreeNode(2);
+        binaryTreeIsBalanced.root.right = new MyTreeNode(3);
+        binaryTreeIsBalanced.root.left.left = new MyTreeNode(4);
+        binaryTreeIsBalanced.root.left.right = new MyTreeNode(5);
+        binaryTreeIsBalanced.root.left.left.left = new MyTreeNode(8);
+        binaryTreeIsBalanced.root.right.right = new MyTreeNode(7);
+        binaryTreeIsBalanced.root.right.left = new MyTreeNode(6);
+
+        Assert.assertTrue(binaryTreeIsBalanced.isBalanced(binaryTreeIsBalanced.root));
+    }
+
+    @Test
+    public void test2() throws Exception {
+        binaryTreeIsBalanced.root = new MyTreeNode<>(1);
+        binaryTreeIsBalanced.root.left = new MyTreeNode(2);
+        binaryTreeIsBalanced.root.left.left = new MyTreeNode(3);
+
+        Assert.assertFalse(binaryTreeIsBalanced.isBalanced(binaryTreeIsBalanced.root));
+    }
+
+    @Test
+    public void test3() throws Exception {
+        binaryTreeIsBalanced.root = new MyTreeNode<>(1);
+        binaryTreeIsBalanced.root.left = new MyTreeNode(2);
+        binaryTreeIsBalanced.root.right = new MyTreeNode(3);
+        binaryTreeIsBalanced.root.left.left = new MyTreeNode(4);
+        binaryTreeIsBalanced.root.left.right = new MyTreeNode(5);
+        binaryTreeIsBalanced.root.left.left.left = new MyTreeNode(6);
+        binaryTreeIsBalanced.root.left.left.right = new MyTreeNode(7);
+
+        Assert.assertFalse(binaryTreeIsBalanced.isBalanced(binaryTreeIsBalanced.root));
+    }
+
+    @Test
+    public void test4() throws Exception {
+        binaryTreeIsBalanced.root = new MyTreeNode<>(1);
+        binaryTreeIsBalanced.root.right = new MyTreeNode(2);
+        binaryTreeIsBalanced.root.right.left = new MyTreeNode(3);
+
+        Assert.assertFalse(binaryTreeIsBalanced.isBalanced(binaryTreeIsBalanced.root));
+    }
+
+    @Test
+    public void test5() throws Exception {
+        binaryTreeIsBalanced.root = new MyTreeNode<>(1);
+        binaryTreeIsBalanced.root.left = new MyTreeNode(2);
+        binaryTreeIsBalanced.root.right = new MyTreeNode(3);
+        binaryTreeIsBalanced.root.left.left = new MyTreeNode(4);
+        binaryTreeIsBalanced.root.left.right = new MyTreeNode(5);
+        binaryTreeIsBalanced.root.right.right = new MyTreeNode(6);
+        binaryTreeIsBalanced.root.right.right.right = new MyTreeNode(7);
+
+        Assert.assertFalse(binaryTreeIsBalanced.isBalanced(binaryTreeIsBalanced.root));
+    }
+
+    @Test
+    public void test6() throws Exception {
+        binaryTreeIsBalanced.root = new MyTreeNode<>(1);
+        binaryTreeIsBalanced.root.left = new MyTreeNode(2);
+        binaryTreeIsBalanced.root.right = new MyTreeNode(3);
+        binaryTreeIsBalanced.root.left.left = new MyTreeNode(4);
+
+        Assert.assertTrue(binaryTreeIsBalanced.isBalanced(binaryTreeIsBalanced.root));
+    }
+}
