371ca92457b470a65441c1880631fe4bfd2ddf73
Aswathy <aswathy.sreenath9@gmail.com>
2018-11-06 16:49:33 +0530

    BT balanced recursive

---
+++ src/test/java/com/abn/dsalgos/challenges/binaryTree/BinaryTreeIsBalancedRecursive.java
@@ -0,0 +1,36 @@
+package com.abn.dsalgos.challenges.binaryTree;
+
+import com.abn.dsalgos.utils.MyTreeNode;
+
+public class BinaryTreeIsBalancedRecursive<T> {
+
+    MyTreeNode<T> root;
+
+    public BinaryTreeIsBalancedRecursive() {
+        root = null;
+    }
+
+    public boolean isBalanced(MyTreeNode<T> root) {
+        return checkHeight(root) != Integer.MIN_VALUE;
+    }
+
+    private int checkHeight(MyTreeNode<T> root) {
+        if (root == null)
+            return -1;
+
+        int leftHeight = checkHeight(root.left);
+        if (leftHeight == Integer.MIN_VALUE)
+            return Integer.MIN_VALUE;
+
+        int rightHeight = checkHeight(root.right);
+        if (rightHeight == Integer.MIN_VALUE)
+            return Integer.MIN_VALUE;
+
+        int heightDiff = leftHeight - rightHeight;
+        if (Math.abs(heightDiff) > 1) {
+            return Integer.MIN_VALUE;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
