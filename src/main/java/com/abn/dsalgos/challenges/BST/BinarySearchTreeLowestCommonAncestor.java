c6c68c8423965e08e983e12236388f263c17c1a7
Aswathy <aswathy.sreenath9@gmail.com>
2018-11-08 17:45:19 +0530

    BST:Lowest common ancestor

---
+++ src/main/java/com/abn/dsalgos/challenges/BST/BinarySearchTreeLowestCommonAncestor.java
@@ -0,0 +1,89 @@
+package com.abn.dsalgos.challenges.BST;
+
+import com.abn.dsalgos.utils.MyBSTNode;
+
+
+public class BinarySearchTreeLowestCommonAncestor<T extends Comparable<T>> {
+
+    MyBSTNode<T> root;
+
+    public BinarySearchTreeLowestCommonAncestor() {
+        root = null;
+    }
+
+    public void insertNode(MyBSTNode<T> node) {
+        if (root == null) {
+            root = node;
+            return;
+        }
+
+        MyBSTNode<T> current = root;
+        MyBSTNode<T> parent = null;
+
+        while(current != null) {
+            parent = current;
+
+            if(node.data.compareTo(current.data) < 0) {
+
+                current = current.left;
+                if(current == null) {
+                    parent.left = node;
+                    return;
+                }
+            } else {
+                current = current.right;
+                if(current == null) {
+                    parent.right = node;
+                    return;
+                }
+            }
+        }
+    }
+
+    public T lowestCommonAncestor(MyBSTNode<T> root,MyBSTNode<T> node1, MyBSTNode<T> node2){
+
+        MyBSTNode<T> temp = root;
+
+        /*
+          Case 1: If root is null or both the nodes passed are same
+         */
+        if(root == null || node1.data.compareTo(node2.data) == 0) {
+            return null;
+        }
+
+        /*
+         Case 2: If either of the node equals root, return root;
+         */
+        if(node1.data.compareTo(root.data) == 0 || node2.data.compareTo(root.data) == 0) {
+            return root.data;
+        }
+
+        /*
+         Case 3: If one of the nodes is greater than the root and another is less than the root,
+          then return root;
+         */
+        if( (node1.data.compareTo(root.data) > 0 && node2.data.compareTo(root.data) < 0) ||
+                (node2.data.compareTo(root.data) > 0 && node1.data.compareTo(root.data) < 0 )) {
+            return root.data;
+        }
+
+        /*
+        If both nodes are less than root, iterate through left tree and find the max of 2
+        If both nodes are greater than root, iterate through right tree and find the max of 2
+         */
+        else  {
+            while(temp != null) {
+                if(temp.data.compareTo(node1.data) > 0 && temp.data.compareTo(node2.data) > 0) {
+                    temp = temp.left;
+                }
+                else if(temp.data.compareTo(node1.data) <0 && temp.data.compareTo(node2.data) < 0) {
+                    temp = temp.right;
+                }
+                else {
+                    return temp.data;
+                }
+            }
+        }
+        return temp.data;
+    }
+}
---
+++ src/test/java/com/abn/dsalgos/challenges/BST/BinarySearchTreeLowestCommonAncestorTest.java
@@ -0,0 +1,36 @@
+package com.abn.dsalgos.challenges.BST;
+
+import com.abn.dsalgos.utils.MyBSTNode;
+import org.testng.annotations.Test;
+
+public class BinarySearchTreeLowestCommonAncestorTest {
+
+    BinarySearchTreeLowestCommonAncestor<Integer> bst = new BinarySearchTreeLowestCommonAncestor<>();
+
+
+    @Test()
+    public void test() throws Exception {
+        MyBSTNode<Integer> node1 = new MyBSTNode<Integer>(40);
+        MyBSTNode<Integer> node2 = new MyBSTNode<Integer>(20);
+        MyBSTNode<Integer> node3 = new MyBSTNode<Integer>(60);
+        MyBSTNode<Integer> node4 = new MyBSTNode<>(10);
+        MyBSTNode<Integer> node5 = new MyBSTNode<>(30);
+        MyBSTNode<Integer> node6 = new MyBSTNode<>(50);
+        MyBSTNode<Integer> node7 = new MyBSTNode<>(70);
+        MyBSTNode<Integer> node8 = new MyBSTNode<>(5);
+        MyBSTNode<Integer> node9 = new MyBSTNode<>(55);
+
+        bst.insertNode(node1);
+        bst.insertNode(node2);
+        bst.insertNode(node3);
+        bst.insertNode(node4);
+        bst.insertNode(node5);
+        bst.insertNode(node6);
+        bst.insertNode(node7);
+        bst.insertNode(node8);
+        bst.insertNode(node9);
+
+        System.out.println(bst.lowestCommonAncestor(node1,node3,node7));
+
+    }
+}
