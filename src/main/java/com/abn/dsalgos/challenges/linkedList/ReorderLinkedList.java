cad25c6d75aed8f354f4dfa5fd524b1f28d05eeb
Aswathy <aswathy.sreenath9@gmail.com>
2019-01-24 14:48:58 +0530

    Reorder Linkedlist

---
+++ src/main/java/com/abn/dsalgos/challenges/linkedList/ReorderLinkedList.java
@@ -0,0 +1,126 @@
+package com.abn.dsalgos.challenges.linkedList;
+
+
+import com.abn.dsalgos.utils.LinkNode;
+import com.abn.dsalgos.utils.Node;
+
+/*
+Given a singly linked list
+    L: L0 → L1 → … → Ln-1 → Ln,
+reorder it to:
+    L0 → Ln → L1 → Ln-1 → L2 → Ln-2 → …
+
+You must do this in-place without altering the nodes’ values.
+ */
+public class ReorderLinkedList<T> {
+
+    private LinkNode<T> first;
+    private LinkNode<T> last;
+    private int size;
+
+
+
+    public LinkNode<T> reorderLinkedList(LinkNode<T> node) {
+
+        LinkNode<T> current = node;
+        LinkNode<T> head1 = node;
+        LinkNode<T> head2 = null;
+        LinkNode<T> fNode = node;
+        LinkNode<T> sNode = node;
+
+        if(node == null) {
+            return null;
+        }
+
+        while(fNode != null && fNode.next != null) {
+
+            sNode = sNode.next;
+            fNode = fNode.next.next;
+        }
+
+        LinkNode<T> tail = sNode;
+
+        /*
+        Reverse the second half of the original list and return the head of that half.
+         */
+        head2 = reverseLinkedList(sNode.next);
+
+        tail.next = null;
+
+        /*
+        Traverse both the half simultaneously and merge alternatively
+         */
+        while(head2 != null) {
+            LinkNode<T> temp1 = head1.next;
+            LinkNode<T> temp2 = head2.next;
+
+            head1.next = head2;
+            head2.next = temp1;
+
+            head1 = temp1;
+            head2 = temp2;
+
+        }
+
+        return current;
+    }
+
+
+    private LinkNode<T> reverseLinkedList(LinkNode<T> headNode) {
+        LinkNode<T> currentNode = headNode;
+        LinkNode<T> prevNode = null;
+        LinkNode<T> nextNode = null;
+
+        while (currentNode != null) {
+            // Getting the next node and storing
+            nextNode = currentNode.next;
+            // linking current node to null (for first case and late to prev nods)
+            currentNode.next = prevNode;
+            //iterating
+            prevNode = currentNode;
+            currentNode = nextNode;
+        }
+        return prevNode;
+    }
+
+    public ReorderLinkedList() {
+        first =null;
+        last = null;
+        size=0;
+    }
+
+    public void insertFirst(T value) {
+        LinkNode<T> newLink = new LinkNode<>(value);
+        if(isEmpty()) {
+            last = newLink;
+        } else {
+            newLink.next = first;
+        }
+        first = newLink;
+        size++;
+    }
+
+    public void insertLast(T value) {
+        LinkNode <T> newLink = new LinkNode<>(value);
+        if(isEmpty()) {
+            first = newLink;
+        } else {
+            last.next = newLink;
+        }
+        last = newLink;
+        size ++;
+
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void print(LinkNode<T> firstNode) {
+        LinkNode<T> n = firstNode;
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+}
---
+++ src/test/java/com/abn/dsalgos/challenges/linkedList/ReorderLinkedListTest.java
@@ -0,0 +1,94 @@
+package com.abn.dsalgos.challenges.linkedList;
+
+import com.abn.dsalgos.utils.LinkNode;
+import org.testng.annotations.Test;
+
+public class ReorderLinkedListTest {
+
+    public ReorderLinkedList<Integer> list = new ReorderLinkedList<>();
+
+    @Test
+    public void test1() throws Exception {
+        LinkNode<Integer> node1 = new LinkNode<>(1);
+        LinkNode<Integer> node2 = new LinkNode<>(2);
+        LinkNode<Integer> node3= new LinkNode<>(3);
+        LinkNode<Integer> node4 = new LinkNode<>(4);
+        LinkNode<Integer> node5 = new LinkNode<>(5);
+        LinkNode<Integer> node6 = new LinkNode<>(6);
+        LinkNode<Integer> node7 = new LinkNode<>(7);
+        LinkNode<Integer> node8 = new LinkNode<>(8);
+        LinkNode<Integer> node9 = new LinkNode<>(9);
+        LinkNode<Integer> node10 = new LinkNode<>(10);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = node8;
+        node8.next = node9;
+        node9.next = node10;
+        node10.next = null;
+
+        list.print(list.reorderLinkedList(node1));
+
+    }
+
+    @Test
+    public void test2() throws Exception{
+        LinkNode<Integer> node1 = new LinkNode<>(1);
+        LinkNode<Integer> node2 = new LinkNode<>(2);
+        LinkNode<Integer> node3= new LinkNode<>(3);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = null;
+
+        list.print(list.reorderLinkedList(node1));
+    }
+
+    @Test
+    public void test3() throws Exception{
+        LinkNode<Integer> node1 = new LinkNode<>(1);
+
+        node1.next = null;
+
+        list.print(list.reorderLinkedList(node1));
+    }
+
+    @Test
+    public void test4() throws Exception{
+        LinkNode<Integer> node1 = new LinkNode<>(1);
+        LinkNode<Integer> node2 = new LinkNode<>(2);
+
+        node1.next = node2;
+        node2.next = null;
+
+        list.print(list.reorderLinkedList(node1));
+    }
+
+    @Test
+    public void test5() throws Exception {
+        LinkNode<Integer> node1 = new LinkNode<>(1);
+        LinkNode<Integer> node2 = new LinkNode<>(2);
+        LinkNode<Integer> node3 = new LinkNode<>(3);
+        LinkNode<Integer> node4 = new LinkNode<>(4);
+        LinkNode<Integer> node5 = new LinkNode<>(5);
+        LinkNode<Integer> node6 = new LinkNode<>(6);
+        LinkNode<Integer> node7 = new LinkNode<>(7);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = null;
+
+        list.print(list.reorderLinkedList(node1));
+
+    }
+}
