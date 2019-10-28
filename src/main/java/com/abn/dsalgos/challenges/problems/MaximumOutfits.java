d274444d7a1775b3c1aee87699c3e33d5987ee9b
Aswathy <aswathy.sreenath9@gmail.com>
2019-10-28 22:07:14 +0530

    Maximum outfit

---
+++ src/main/java/com/abn/dsalgos/challenges/problems/MaximumOutfits.java
@@ -0,0 +1,41 @@
+package com.abn.dsalgos.challenges.problems;
+
+
+import java.util.List;
+
+/*
+Arbitary shopping
+    A shopper goes to shop and picks an outfit.Later shopper buys all consecutive outfits starting with that outfit up to
+    nth outfit as long as money is available. Determine max number of outfits shopper can buy.
+
+    n = 7
+    [2,3,5,1,1,2,1]
+    money = 5
+
+    no of subarrays with sum = [2,3],[5],[1,1,2,1]
+    o/p : longest one : 4
+
+ */
+public class MaximumOutfits {
+
+    public static int getMaximumOutfits(List<Integer> outfits,int money) {
+        int[] array = outfits.stream().mapToInt(i->i).toArray();
+        int size = array.length;
+        int sum = 0;
+        int count = 0;
+        int maxCount = 0;
+
+        for(int i=0;i<size;i++){
+            if((sum + array[i])<= money) {
+                sum+= array[i];
+                count++;
+            }
+
+            else if(sum!=0) {
+                sum = sum - array[i-count] +array[i];
+            }
+            maxCount = Math.max(count,maxCount);
+        }
+        return maxCount;
+    }
+}
---
+++ src/main/java/com/abn/dsalgos/challenges/problems/MeanderingArray.java
@@ -0,0 +1,37 @@
+package com.abn.dsalgos.challenges.problems;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+Array = [-1,1,2,3,-5]
+Meandering array = [3,-5,2,-1,1]
+                  [first_largest, first_smallest, second_largest,second_smallest, ....]
+ */
+public class MeanderingArray {
+
+    public static List<Integer> meanderingArray(List<Integer> input) {
+        int[] array = input.stream().mapToInt(i->i).toArray();
+        Arrays.sort(array);
+
+        int size = array.length;
+        int[] temp = new int[size];
+
+        int index=0;
+
+        for(int i=0, j= size-1; i<=size/2 || j>size/2;i++,j--) {
+            if(index<size) {
+                temp[index] = array[j];
+                index++;
+            }
+
+            if(index <size) {
+                temp[index] = array[i];
+                index++;
+            }
+        }
+        return Arrays.stream(array).boxed().collect(Collectors.toList());
+    }
+}
