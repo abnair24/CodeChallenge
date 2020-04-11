e0d4909848ffa8825d31542013cada695b8ebc3c
Aswathy Nair <aswathy.sreenath9@gmail.com>
2020-04-11 15:46:51 +0530

    Middle linked

---
+++ src/main/java/com/abn/dsalgos/leetcode30Day/MiddleOfLinkedList.java
@@ -0,0 +1,20 @@
+package com.abn.dsalgos.leetcode30Day;
+
+import com.abn.dsalgos.utils.ListNode;
+
+public class MiddleOfLinkedList {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode fastPtr = head;
+        ListNode slowPtr = head;
+
+        while(fastPtr != null && fastPtr.next !=null) {
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+        }
+
+        return slowPtr;
+    }
+}
+
+
---
+++ src/test/java/com/abn/dsalgos/leetcode30Day/MiddleOfLinkedListTest.java
@@ -0,0 +1,36 @@
+package com.abn.dsalgos.leetcode30Day;
+
+import com.abn.dsalgos.utils.ListNode;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.w3c.dom.ls.LSException;
+
+public class MiddleOfLinkedListTest {
+
+    MiddleOfLinkedList middleOfLinkedList;
+
+    @BeforeMethod
+    public void before() throws Exception {
+        middleOfLinkedList = new MiddleOfLinkedList();
+    }
+
+    @Test
+    public void test1() throws Exception {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+       // ListNode l6 = new ListNode(6);
+
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+       // l5.next = l6;
+
+        Assert.assertEquals(middleOfLinkedList.middleNode(l1).val,3);
+
+    }
+}
