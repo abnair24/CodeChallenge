9a075ff5d9c3beb1e3da32c8ed22cc54244d86b1
Aswathy Nair <aswathy.sreenath9@gmail.com>
2020-12-21 19:37:16 +0530

    Space optimization

--- src/main/java/com/abn/dsalgos/challenges/problems/RotateArrayKtimes.java
+++ src/main/java/com/abn/dsalgos/challenges/problems/RotateArrayKtimes.java
@@ -1,11 +1,18 @@
 package com.abn.dsalgos.challenges.problems;
 
-import java.util.Arrays;
-import java.util.HashMap;
+/*
+Given an integer, rotate array k times based on left or right as passed in parameter
+
+input = [1,2,3,4,5,6,7], k = 5, shift = left
+output = [6,7,1,2,3,4,5]
+
+input = [1,2,3,4,5,6,7] k = 4, shift = right
+output = [4,5,6,7,1,2,3]
+ */
 
 public class RotateArrayKtimes {
 
-
+    /*
     public int[] rotateArray(int[] array,int shift) {
 
         int length = array.length;
@@ -26,6 +33,47 @@ public class RotateArrayKtimes {
             temp[i]= array[i - (length-x)];
         }
         return temp;
+    }
+
+     */
+
+    // O(n) time and O(1) space
+
+    // shiftSide : true -> left, false -> right
+
+    public int[] rotateArray(int[] array, int shift, boolean shiftSide) {
+        int len = array.length;
+        if(len == 0 || len == 1) {
+            return array;
+        }
+
+        int x = shift % len;
+        if(x == 0) {
+            return array;
+        }
+
+        reverse(array, 0, len - 1);
+
+        if(shiftSide) {
+            reverse(array, 0, len - x - 1);
+            reverse(array, len - x, len - 1);
+        } else {
+            reverse(array, 0, x-1);
+            reverse(array, x, len-1);
+        }
+
+        return array;
+    }
+
+    private void reverse(int[] array, int start, int end) {
+
+        while(start <= end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start ++;
+            end --;
+        }
     }
 }
 
--- src/test/java/com/abn/dsalgos/challenges/problems/RotateArrayKtimesTest.java
+++ src/test/java/com/abn/dsalgos/challenges/problems/RotateArrayKtimesTest.java
@@ -11,14 +11,14 @@ public class RotateArrayKtimesTest {
         int[] array = new int[] {1,2,3,4,5,6,7};
         RotateArrayKtimes rotateArrayKtimes = new RotateArrayKtimes();
 
-        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,7),array);
+        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,7, true),array);
     }
 
     @Test
     public void rotateByLengthMultiplierTimesTest() throws Exception {
         int[] array = new int[] {1,2,3,4,5,6,7};
         RotateArrayKtimes rotateArrayKtimes = new RotateArrayKtimes();
-        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,49),array);
+        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,49, true),array);
     }
 
     @Test
@@ -26,7 +26,7 @@ public class RotateArrayKtimesTest {
         int[] array = new int[] {1,2,3,4,5,6,7};
         int[] expectedArray = new int[] {4,5,6,7,1,2,3};
         RotateArrayKtimes rotateArrayKtimes = new RotateArrayKtimes();
-        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,3),expectedArray);
+        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,3, true),expectedArray);
     }
 
     @Test
@@ -34,21 +34,67 @@ public class RotateArrayKtimesTest {
         int[] array = new int[] {1,2,3,4,5,6,7};
         int[] expectedArray = new int[] {6,7,1,2,3,4,5};
         RotateArrayKtimes rotateArrayKtimes = new RotateArrayKtimes();
-        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,12),expectedArray);
+        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,12, true),expectedArray);
     }
 
     @Test
     public void rotateEmptyArrayTest() throws Exception {
         int[] array = new int[] {};
         RotateArrayKtimes rotateArrayKtimes = new RotateArrayKtimes();
-        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,12),array);
+        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,12, true),array);
     }
 
     @Test
     public void rotateArrayOfSizeOne() throws Exception {
         int[] array = new int[] {1};
         RotateArrayKtimes rotateArrayKtimes = new RotateArrayKtimes();
-        Assert.assertEquals(rotateArrayKtimes.rotateArray(array, 2),array);
+        Assert.assertEquals(rotateArrayKtimes.rotateArray(array, 2, true),array);
+    }
+
+
+    @Test
+    public void rotateByLengthTimesRightTest() throws Exception {
+        int[] array = new int[] {1,2,3,4,5,6,7};
+        RotateArrayKtimes rotateArrayKtimes = new RotateArrayKtimes();
+
+        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,7, false),array);
+    }
+
+    @Test
+    public void rotateByLengthMultiplierTimesRightTest() throws Exception {
+        int[] array = new int[] {1,2,3,4,5,6,7};
+        RotateArrayKtimes rotateArrayKtimes = new RotateArrayKtimes();
+        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,49, false),array);
+    }
+
+    @Test
+    public void rotateByLessThanLengthTimesRightTest() throws Exception {
+        int[] array = new int[] {1,2,3,4,5,6,7};
+        int[] expectedArray = new int[] {5,6,7,1,2,3,4};
+        RotateArrayKtimes rotateArrayKtimes = new RotateArrayKtimes();
+        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,3, false),expectedArray);
+    }
+
+    @Test
+    public void rotateByGreaterThanLengthTimesRightTest() throws Exception {
+        int[] array = new int[] {1,2,3,4,5,6,7};
+        int[] expectedArray = new int[] {3,4,5,6,7,1,2};
+        RotateArrayKtimes rotateArrayKtimes = new RotateArrayKtimes();
+        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,12, false),expectedArray);
+    }
+
+    @Test
+    public void rotateEmptyArrayRightTest() throws Exception {
+        int[] array = new int[] {};
+        RotateArrayKtimes rotateArrayKtimes = new RotateArrayKtimes();
+        Assert.assertEquals(rotateArrayKtimes.rotateArray(array,12, false),array);
+    }
+
+    @Test
+    public void rotateArrayOfSizeOneRightTest() throws Exception {
+        int[] array = new int[] {1};
+        RotateArrayKtimes rotateArrayKtimes = new RotateArrayKtimes();
+        Assert.assertEquals(rotateArrayKtimes.rotateArray(array, 2, false),array);
     }
 
 }
