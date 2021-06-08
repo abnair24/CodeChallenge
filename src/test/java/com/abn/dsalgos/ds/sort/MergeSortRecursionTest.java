894729c012fbbe76c81bddc30c18d8f6f195f578
Aswathy <aswathy.sreenath9@gmail.com>
2021-06-08 19:22:34 +0530

    Recursion II

--- src/test/java/com/abn/dsalgos/ds/sort/MergeSortRecursionTest.java
+++ src/test/java/com/abn/dsalgos/ds/sort/MergeSortRecursionTest.java
@@ -18,9 +18,9 @@ public class MergeSortRecursionTest {
 
         List<Object[]> list = Lists.newLinkedList();
 
-//        list.add(new Object[] {new int[] {5, 6, 1, 2, 4, 3, 5, 8}, new int[] {1, 2, 3, 4, 5, 5, 6, 8}});
-//        list.add(new Object[] {new int[] {7, 1, 3}, new int[] {1, 3, 7}});
-//        list.add(new Object[] {new int[] {1}, new int[] {1}});
+        list.add(new Object[] {new int[] {5, 6, 1, 2, 4, 3, 5, 8}, new int[] {1, 2, 3, 4, 5, 5, 6, 8}});
+        list.add(new Object[] {new int[] {7, 1, 3}, new int[] {1, 3, 7}});
+        list.add(new Object[] {new int[] {1}, new int[] {1}});
         list.add(new Object[] {new int[] {-2, 3, -5}, new int[] {-5, -2, 3}});
 
 
