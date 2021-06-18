b79c1e6d2e683714514cc0cc01d3a7407731a7c0
Aswathy Nair <8415806+abnair24@users.noreply.github.com>
2021-06-18 23:43:25 +0530

    Trie problems (#13)

---
+++ src/main/java/com/abn/dsalgos/algo/DFS/BinaryTreeExtraEdgeRemoveRecursion.java
@@ -0,0 +1,53 @@
+package com.abn.dsalgos.algo.DFS;
+
+import com.abn.dsalgos.utils.MyTreeNode;
+import com.abn.dsalgos.utils.TreeNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+Given a binary tree, where an arbitary node has 2 parents
+i.e two nodes in the tree have the same child. Identify the defective node and remove an extra edge to fix the tree.
+
+       1
+	  / \
+	 2   3   =>
+	/ \ /
+   4   5
+
+
+     1			       1
+    / \			      / \
+   2   3    or	     2   3
+  / \ 			    /   /
+ 4   5		       4   5
+
+ */
+public class BinaryTreeExtraEdgeRemoveRecursion {
+
+    public MyTreeNode<Integer> removeExtraNode(MyTreeNode<Integer> root) {
+
+        // DFS and Store the visited nodes in hashset.
+        // Duplicate addition returns false and store that node as null
+
+        Set<MyTreeNode<Integer>> hs = new HashSet<>();
+        if(root == null) {
+            return null;
+        }
+
+        return removeNode(root, hs);
+    }
+
+    private MyTreeNode<Integer> removeNode(MyTreeNode<Integer> root, Set<MyTreeNode<Integer>> hs) {
+
+        if(root == null || !hs.add(root)) {
+            return null;
+        }
+
+        removeNode(root.left, hs);
+        removeNode(root.right, hs);
+
+        return root;
+    }
+}
--- src/main/java/com/abn/dsalgos/challenges/BST/BinarySearchTreeLowestCommonAncestor.java
+++ src/main/java/com/abn/dsalgos/challenges/BST/BinarySearchTreeLowestCommonAncestor.java
@@ -2,88 +2,22 @@ package com.abn.dsalgos.challenges.BST;
 
 import com.abn.dsalgos.utils.MyBSTNode;
 
+public class BinarySearchTreeLowestCommonAncestor {
 
-public class BinarySearchTreeLowestCommonAncestor<T extends Comparable<T>> {
+    public MyBSTNode<Integer> lowestCommonAncestor(MyBSTNode<Integer> root, MyBSTNode<Integer> p, MyBSTNode<Integer> q) {
 
-    MyBSTNode<T> root;
-
-    public BinarySearchTreeLowestCommonAncestor() {
-        root = null;
-    }
-
-    public void insertNode(MyBSTNode<T> node) {
         if (root == null) {
-            root = node;
-            return;
-        }
-
-        MyBSTNode<T> current = root;
-        MyBSTNode<T> parent;
-
-        while(current != null) {
-            parent = current;
-
-            if(node.data.compareTo(current.data) < 0) {
-
-                current = current.left;
-                if(current == null) {
-                    parent.left = node;
-                    return;
-                }
-            } else {
-                current = current.right;
-                if(current == null) {
-                    parent.right = node;
-                    return;
-                }
-            }
-        }
-    }
-
-    public T lowestCommonAncestor(MyBSTNode<T> root,MyBSTNode<T> node1, MyBSTNode<T> node2){
-
-        MyBSTNode<T> temp = root;
-
-        /*
-          Case 1: If root is null or both the nodes passed are same
-         */
-        if(root == null || node1.data.compareTo(node2.data) == 0) {
             return null;
         }
 
-        /*
-         Case 2: If either of the node equals root, return root;
-         */
-        if(node1.data.compareTo(root.data) == 0 || node2.data.compareTo(root.data) == 0) {
-            return root.data;
-        }
+        if (p.data > root.data && q.data > root.data) {
 
-        /*
-         Case 3: If one of the nodes is greater than the root and another is less than the root,
-          then return root;
-         */
-        if( (node1.data.compareTo(root.data) > 0 && node2.data.compareTo(root.data) < 0) ||
-                (node2.data.compareTo(root.data) > 0 && node1.data.compareTo(root.data) < 0 )) {
-            return root.data;
-        }
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (p.data < root.data && q.data < root.data) {
 
-        /*
-        If both nodes are less than root, iterate through left tree and find the max of 2
-        If both nodes are greater than root, iterate through right tree and find the max of 2
-         */
-        else  {
-            while(temp != null) {
-                if(temp.data.compareTo(node1.data) > 0 && temp.data.compareTo(node2.data) > 0) {
-                    temp = temp.left;
-                }
-                else if(temp.data.compareTo(node1.data) <0 && temp.data.compareTo(node2.data) < 0) {
-                    temp = temp.right;
-                }
-                else {
-                    return temp.data;
-                }
-            }
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return root;
         }
-        return temp.data;
     }
 }
