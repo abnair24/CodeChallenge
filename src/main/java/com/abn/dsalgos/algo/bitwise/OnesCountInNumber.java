beccea6b63e89d038d7d9c6b567292c501bb697c
Aswathy Nair <aswathy.sreenath9@gmail.com>
2021-06-21 22:06:09 +0530

    cleanup

--- src/main/java/com/abn/dsalgos/algo/bitwise/OnesCountInNumber.java
+++ src/main/java/com/abn/dsalgos/algo/bitwise/OnesCountInNumber.java
@@ -4,7 +4,11 @@ public class OnesCountInNumber {
 
     public int countOnes(int num) {
         int count = 0;
-        while(num > 0) {
+
+        // consider the case of n = 11111111111111111111111111111101, java represents as 2's compliment => -3.
+        // while condition should check for not equals to 0 instead of greater than 0.
+
+        while(num != 0) {
 
             // num & num - 1 sets off each one in the iteration leaving num as 0 at the end.
             num = num & (num -1);
