0762dd18d37361e8897f1848b176b0407e3d7cdc
Aswathy Nair <aswathy.sreenath9@gmail.com>
2021-07-04 17:27:40 +0530

    tree problems

--- src/main/java/com/abn/dsalgos/algo/binarySearch/FrequencyInSortedArray.java
+++ src/main/java/com/abn/dsalgos/algo/binarySearch/FrequencyInSortedArray.java
@@ -53,9 +53,4 @@ public class FrequencyInSortedArray {
 
         return result1 - result + 1;
     }
-
-    public static void main(String[] args) {
-        FrequencyInSortedArray freq = new FrequencyInSortedArray();
-        freq.frequency(new int[] {1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8, 9, 10, 11}, 4);
-    }
 }
