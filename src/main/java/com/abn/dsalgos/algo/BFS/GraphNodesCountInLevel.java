36ca9cf4d2f0b5928d8a8c95db3d8e62e075e6e0
Aswathy Nair <aswathy.sreenath9@gmail.com>
2021-07-12 07:42:47 +0530

    graph tests

--- src/main/java/com/abn/dsalgos/algo/BFS/GraphNodesCountInLevel.java
+++ src/main/java/com/abn/dsalgos/algo/BFS/GraphNodesCountInLevel.java
@@ -6,6 +6,9 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/*
+Calculate the Number of Nodes in a Given Graph Level
+ */
 public class GraphNodesCountInLevel {
 
     boolean[] isVisited;
@@ -24,11 +27,10 @@ public class GraphNodesCountInLevel {
         while (!queue.isEmpty()) {
             int nodeCount = queue.size();
 
+            if (l == level) {
+                count = nodeCount;
+            }
             while (nodeCount > 0) {
-                if (l == level) {
-                    count = nodeCount;
-                }
-
                 int temp = queue.poll();
                 Iterator<Integer> iterator = graph.getAdjacentList()[temp].listIterator();
                 while (iterator.hasNext()) {
@@ -38,22 +40,10 @@ public class GraphNodesCountInLevel {
                         isVisited[n] = true;
                     }
                 }
-                nodeCount -- ;
+                nodeCount--;
             }
             l++;
         }
         return count;
     }
-
-        public static void main (String[]args){
-            Graph graph = new Graph(6);
-            graph.addEdge(0, 1);
-            graph.addEdge(0, 2);
-            graph.addEdge(1, 3);
-            graph.addEdge(2, 3);
-            graph.addEdge(3, 5);
-            graph.addEdge(2, 4);
-
-            System.out.println(new GraphNodesCountInLevel().nodeCountInLevel(3, 0, graph));
-        }
-    }
+}
