7c3b6039ef3ddd775a1cca86b467ab4c12624de9
Aswathy Nair <aswathy.sreenath9@gmail.com>
2022-05-14 21:27:42 +0530

    MaximumSum sliding window

--- src/main/java/com/abn/dsalgos/algo/slidingWindo/MaxValueInWindow.java
+++ src/main/java/com/abn/dsalgos/algo/slidingWindo/MaxValueInWindow.java
@@ -6,37 +6,24 @@ the very left of the array to the very right. You can only see the k numbers
 in the window. Each time the sliding window moves right by one position.
  */
 
-//TODO
 public class MaxValueInWindow {
 
-    public int[] maxSlidingWindow(int[] arr, int k) {
-        int[] array = new int[arr.length - k + 1];
+    public int maxSlidingWindow(int[] arr, int k) {
 
-        int start = 0;
-        int max = 0;
-        int count = 0;
+        int windowStart = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
 
-        if (k == 1) {
-            return arr;
-        }
+        for(int windowEnd = 0; windowEnd < arr.length; windowEnd ++) {
 
-        for (int i = 0; i < arr.length; i++) {
+            sum += arr[windowEnd];
 
-            max = Math.max(max, arr[i]);
-
-            int len = i - start + 1;
-            if (len == k) {
-                array[start] = max;
-                start++;
-                max = arr[start];
+            if(windowEnd >= k - 1) {
+                maxSum = Math.max(maxSum, sum);
+                sum = sum - arr[windowStart];
+                windowStart ++;
             }
         }
-        return array;
+        return maxSum;
     }
-
-    public static void main(String[] args) {
-        MaxValueInWindow maxValueInWindow = new MaxValueInWindow();
-        maxValueInWindow.maxSlidingWindow(new int[] {1, 6, 5, 4, 2, 7, 9}, 3);
-    }
-
 }
