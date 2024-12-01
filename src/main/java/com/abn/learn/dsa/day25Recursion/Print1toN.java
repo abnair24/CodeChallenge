704a6b88f33a73e9fab459c0fe1d2cb4889e8bc7
Aswathy Nair <aswathy.sreenath9@gmail.com>
2024-12-01 16:27:18 +0530

    Recursion

---
+++ src/main/java/com/abn/learn/dsa/day25Recursion/Print1toN.java
@@ -0,0 +1,22 @@
+package com.abn.learn.dsa.day25Recursion;
+
+public class Print1toN {
+
+    public void rec(int A) {
+        if(A == 0) {
+            return;
+        }
+        rec(A - 1);
+        System.out.print(A + " ");
+    }
+
+    public void solve(int A) {
+       rec(A);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Print1toN print1toN = new Print1toN();
+        print1toN.solve(9);
+    }
+}
